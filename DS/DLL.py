3506475641791c38388011674e332af78a0e7fb5
William Neyland <rileyneyland7@gmail.com>
2024-04-06 10:13:36 -0500

    Adds file structure for Hash Map and DLL

---
+++ DS/DLL.py
@@ -0,0 +1,2 @@
+# Doubly Linked List Implementation
+# DLL used for collision chaining in hash map
---
+++ DS/HashMap.py
@@ -0,0 +1,20 @@
+# Hash Map Implementation
+
+class HashMap:
+    def __init__(self):
+        pass
+
+    def getHash(self, key):
+        pass
+
+    def add(self, key, value):
+        pass
+
+    def get (self, key):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def print(self):
+        pass
