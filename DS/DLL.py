5cf010432b52bedfc148ec88fd23bf695e02e744
William Neyland <rileyneyland7@gmail.com>
2024-04-07 22:50:45 -0500

    Adds PushBack method to DLL.py

--- DS/DLL.py
+++ DS/DLL.py
@@ -21,16 +21,41 @@ class DLL:
             self.tail = NewNode
             NewNode.prev = None
 
+    def PushBack(self, NewData):
+        NewNode = PackageNode(NewData)
+        NewNode.prev = self.tail
+
+        if self.tail == None: # Executes if the DLL is empty
+            self.head = NewNode
+            self.tail = NewNode
+            NewNode.next = None
+
+        else: # Executes if DLL is not empty
+            self.tail.next = NewNode
+            NewNode.next = None
+            self.tail = NewNode # Sets new Node as last element in DLL
+
     def Print(self):
         currentNode = self.head
         while currentNode != None:
             print(currentNode.data)
             currentNode = currentNode.next
 
+testDLL = DLL()
+myTuple1 = (1, "195 W Oakland Ave", "Salt Lake City", 84115, "#######", 21, "HUB")
+myTuple2 = (2, "195 W Oakland Ave", "Salt Lake City", 84115, "#######", 21, "HUB")
+myTuple3 = (3, "195 W Oakland Ave", "Salt Lake City", 84115, "#######", 21, "HUB")
+
+testDLL.PushFront(NewData=myTuple1)
+testDLL.PushFront(NewData=myTuple2)
+testDLL.PushFront(NewData=myTuple3)
+
+testDLL.Print()
+
 # Methods for DLL: 
 # 
 # PushFront - DONE
-# PushBack
+# PushBack - DONE
 # GetNode
 # Print - DONE
 # Delete
