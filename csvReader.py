7742067606ee9e1090b5f14e3c3989d59f6eb679
William Neyland <rileyneyland7@gmail.com>
2024-04-14 15:53:25 -0500

    Adds loadHashMap() method to csvReader.py class

--- csvReader.py
+++ csvReader.py
@@ -8,15 +8,28 @@ class PackageDataReader:
 
     def print(self):
         for lines in self.csvReader:
-            print(lines)
+            print(lines[0])
+
+    def loadHashMap(self):
+        next(self.csvReader, None) # Skips header row of csv Data
+        newMap = HashMap()
+        for lines in self.csvReader:
+            newMap.add(key=lines[0], data=lines)
 
     def close(self):
-        self.csvFile.close()
+        self.csvFile.close() # Closes reader object after data has been loaded into the Hash Map
 
 
 test = PackageDataReader()
-#test.print()
-#test.close()
 
-testMap = HashMap()
-testMap.print()
+test.loadHashMap()
+test.close()
+
+# data parameter is a tuple containing the following:
+# PackageID - int 0
+# D_Address - str 1
+# D_City - str 2
+# D_ZipCode - int 3
+# D_Deadline - str 4
+# Weight - int 5
+# D_Status - str 6
