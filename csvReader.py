2964dd235eb705942f42d2e67170206b934c6278
William Neyland <rileyneyland7@gmail.com>
2024-04-14 16:40:25 -0500

    Adds initial empty driver file main.py.

--- csvReader.py
+++ csvReader.py
@@ -12,24 +12,17 @@ class PackageDataReader:
 
     def loadHashMap(self):
         next(self.csvReader, None) # Skips header row of csv Data
-        newMap = HashMap()
+        #newMap = HashMap()
         for lines in self.csvReader:
-            newMap.add(key=lines[0], data=lines)
+            testMap.add(key=lines[0], data=lines)
 
     def close(self):
         self.csvFile.close() # Closes reader object after data has been loaded into the Hash Map
 
-
+# Remove the below Code once main driver file is implemented
 test = PackageDataReader()
+testMap = HashMap()
 
 test.loadHashMap()
 test.close()
-
-# data parameter is a tuple containing the following:
-# PackageID - int 0
-# D_Address - str 1
-# D_City - str 2
-# D_ZipCode - int 3
-# D_Deadline - str 4
-# Weight - int 5
-# D_Status - str 6
+testMap.print()
