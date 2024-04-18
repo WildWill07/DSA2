281aafc593e415dbfc0a82b1818835282c22b1bf
William Neyland <rileyneyland7@gmail.com>
2024-04-18 13:08:01 -0500

    Adds truck.py file which contains Truck class

---
+++ truck.py
@@ -0,0 +1,10 @@
+class Truck:
+    def __init__(self, capacity, speed, packageLoad, mileage, address, departTime):
+        pass
+
+    # Speed - 18 mph
+    # capacity - 16
+    # packageLoad
+    # mileage
+    # address
+    # departTime - 8am earliest
