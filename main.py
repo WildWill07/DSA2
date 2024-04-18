8b91d7b274ce3eaf79b59bb70423762b425643e4
William Neyland <rileyneyland7@gmail.com>
2024-04-18 15:11:36 -0500

    Adds functionality for getDistance() and getAddressIndex().

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from DS.HashMap import HashMap
-from csvReader import *
 from truck import Truck
 import csv
 
+# imports necessary csv data and saves data in a list 2D list object
 with open("CSV/PackageData.csv") as file:
     csvPackage = csv.reader(file)
     csvPackage = list(csvPackage)
@@ -15,36 +15,27 @@ with open("CSV/AddressTable.csv") as file:
     csvAddress = csv.reader(file)
     csvAddress = list(csvAddress)
 
+# Searches csvAddress data for a matching address and then returns corresponding address index
+def getAddressIndex(address):
+    for x in range(len(csvAddress)):
+        if csvAddress[x][2] == address:
+            return int(csvAddress[x][0])
+        
+# Takes two address indexes and returns the distance between the two nodes listed in the csvDistance Adj Matrix (2D list)
+# If value is empty then swap axis
+def getDistance(add1, add2):
+    distance = csvDistance[add1][add2]
+    if csvDistance[add1][add2] == '':
+        distance = csvDistance[add2][add1]
+    return float(distance)
 
 def main():
     packageHashMap = HashMap()
     startAddress = "4001 South 700 East" # Hub Address
+    someAddress = "4300 S 1300 E"
 
     packageHashMap.loadHashMap(csvPackage)
-    packageHashMap.print()
-    #findMinDistance(addressReader ,startAddress)
-    
-
-def getDistance(address1, address2):
-    # Find index for each address in the addressReader list
-    # Use returned indexes to access value in distanceReader 2D array
-    # If element accessed == '' flip column & row indexes
-    # return value
-    pass
-
-def findMinDistance(pAddressReader, pStartAddress):
-    # Begin at current address
-    # Find address index in addressTable.csv
-    addressReader = pAddressReader
-    currentAddress = pStartAddress 
-    idx = addressReader.getIndex(currentAddress)
-    print(idx)
-    
-    pass
-
-def getAddressIndex(address):
-    pass
-
+    print(getDistance(getAddressIndex(startAddress), getAddressIndex(someAddress)))
 
 if __name__ == "__main__":
     main()
